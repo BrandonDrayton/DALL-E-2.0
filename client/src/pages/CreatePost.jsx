9a3a48ab557bfa5c16fbecb4b31ee89718913a80
Brandon Drayton <brandondrayton@gmail.com>
2023-01-30 06:22:54 -0500

    FE: Prompt, Generate, Share button logic complete

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,13 +6,20 @@ import { getRandomPrompt } from "../utils";
 import { FormField, Loader } from "../components";
 const CreatePost = () => {
   const navigate = useNavigate();
-  const [form, setform] = useState({ name: "", prompt: "", photo: "" });
+  const [form, setForm] = useState({ name: "", prompt: "", photo: "" });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
+
+  const generateImage = () => {};
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -35,7 +42,7 @@ const CreatePost = () => {
             handleChange={handleChange}
           />
           <FormField
-            labelName="Your Name"
+            labelName="Prompt"
             type="text"
             name="prompt"
             placeholder="A 3D render of a rainbow colored hot air balloon flying above a reflective lake"
@@ -44,6 +51,47 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-black font-medium rounded-md text-sm w-full px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            ** Once you have created the image you want, you can share it with
+            others in the community **
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#0c0c0f] font-medium rounded-md text-sm w-full px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "Share with the Community"}
+          </button>
         </div>
       </form>
     </section>
