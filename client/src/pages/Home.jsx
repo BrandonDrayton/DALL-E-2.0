aeccfef5ef7db2bbcbc3cd5b7d8a99b5396eb22f
Brandon Drayton <brandondrayton@gmail.com>
2023-02-06 17:46:40 -0500

    Slight loader fix for user, render takes too long

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -68,7 +68,8 @@ const Home = () => {
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">
-          The Community Showcase
+          The Community Showcase{" "}
+          {loading ? "(Some times Render needs time to wake up)" : null}
         </h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
           <nobr>
